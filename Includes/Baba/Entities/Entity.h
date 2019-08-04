1a6ce081e21dccaaf44d789ad129c0eb16130581
JYPark09 <jyp10987@gmail.com>
2019-08-04 13:03:46 +0900

    feat(entity): [WIP] Create Entity class

---
+++ Includes/Baba/Entities/Entity.h
@@ -0,0 +1,25 @@
+// Copyright(c) 2019 Junyeong Park
+
+#ifndef BABA_ENTITY_H
+#define BABA_ENTITY_H
+
+#include <Baba/Enums/EntityType.h>
+
+namespace Baba
+{
+//!
+//! \brief Entity interface that conists level
+//!
+class Entity
+{
+ public:
+    //! Default destructor
+    virtual ~Entity() = default;
+
+    //! Return entity type
+    //! \return returned entity type
+	virtual EntityType GetType() const = 0;
+};
+}  // namespace Baba
+
+#endif  // BABA_ENTITY_H
