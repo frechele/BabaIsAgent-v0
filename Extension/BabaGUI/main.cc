bc3a6438ba19b889add8bea9c8aadc0bece4a1f3
JYPark09 <jyp10987@gmail.com>
2019-08-04 15:20:51 +0900

    feat(gui): Add gui window

--- Extension/BabaGUI/main.cc
+++ Extension/BabaGUI/main.cc
@@ -1,6 +1,30 @@
-#include <iostream>
+// Copyright(c) 2019 Junyeong Park
+
+#include <SFML/Graphics.hpp>
+
+#include <spdlog/spdlog.h>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    spdlog::info("BabaIsYou GUI");
+
+    sf::RenderWindow window(sf::VideoMode(680, 480), "Baba Is You",
+                            sf::Style::Titlebar | sf::Style::Close);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                spdlog::info("Window closed");
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        window.display();
+    }
 }
