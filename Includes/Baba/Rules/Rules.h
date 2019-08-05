1e565c52ef57bc4343a6a0d90a630da38cb4accf
HyeonSu Kim <rla92233@naver.com>
2019-08-05 19:56:17 +0900

    impl: Implement AddBaseRule and DeleteRule

--- Includes/Baba/Rules/Rules.h
+++ Includes/Baba/Rules/Rules.h
@@ -1,10 +1,12 @@
-// Copyright(c) 2019 Junyeong Park
+// Copyright(c) 2019 Junyeong Park, Hyeonsu Kim
 
 #ifndef BABA_RULES_H
 #define BABA_RULES_H
 
-#include <map>
-#include <vector>
+#include <Baba/Rules/Rule.h>
+
+#include <list>
+#include <string_view>
 
 namespace Baba
 {
@@ -22,6 +24,20 @@ class GameRules final
     ~GameRules() = default;
 
     GameRules& GetInstance();
+
+    //! Add base rule.
+    //! \param target target's type.
+    //! \param verb verb's type.
+    //! \param effect effect's type.
+    //! \return id of rule.
+    const int AddBaseRule(std::string_view target, std::string_view verb, std::string_view effect);
+
+    //! Delete rule.
+    //! \param ruleID id of rule to delete.
+    void DeleteRule(const int& ruleID);
+
+    int ruleCount = 0;
+    std::list<Rule> rules_;
 };
 }  // namespace Baba
 
--- Sources/Baba/Rules/Rules.cc
+++ Sources/Baba/Rules/Rules.cc
@@ -9,4 +9,25 @@ GameRules& GameRules::GetInstance()
     static GameRules instance;
     return instance;
 }
+
+const int GameRules::AddBaseRule(std::string_view target, std::string_view verb, std::string_view effect)
+{
+    rules_.emplace_back(target, verb, effect, ruleCount);
+    ++ruleCount;
+}
+
+void GameRules::DeleteRule(const int& ruleID)
+{
+    for (auto i = rules_.begin(); i != rules_.end(); ++i)
+    {
+        if (i->ruleID_ == ruleID)
+        {
+            i = rules_.erase(i);
+        }
+        else
+        {
+            ++i;
+        }
+    }
+}
 }  // namespace Baba
