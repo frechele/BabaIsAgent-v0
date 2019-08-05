569b63af2144918f3d9bbac77a51bde7fcd2c057
HyeonSu Kim <rla92233@naver.com>
2019-08-05 14:29:04 +0900

    refactor: Rename Entity to Object

---
+++ Includes/Baba/Objects/Object.h
@@ -0,0 +1,34 @@
+// Copyright(c) 2019 Junyeong Park, Hyeonsu Kim
+
+#ifndef BABA_OBJECT_H
+#define BABA_OBJECT_H
+
+#include <Baba/Enums/ObjectType.h>
+
+namespace Baba
+{
+//!
+//! \brief Object that conists level
+//!
+class Object
+{
+ public:
+    //! Constructor
+    Object();
+
+    //! Constructor with ObjectType
+    Object(ObjectType type);
+
+    //! Default destructor
+    virtual ~Object() = default;
+
+    //! Return entity type
+    //! \return returned entity type
+    virtual ObjectType GetType() const = 0;
+
+ private:
+    ObjectType type_;
+};
+}  // namespace Baba
+
+#endif  // BABA_OBJECT_H
