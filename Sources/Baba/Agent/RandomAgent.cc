757a31b94ffb63ec9e0593245f76d2bb70f39128
JYPark09 <jyp10987@gmail.com>
2019-08-05 22:15:31 +0900

    fix: Fix GetAction method at RandomAction

--- Sources/Baba/Agent/RandomAgent.cc
+++ Sources/Baba/Agent/RandomAgent.cc
@@ -11,6 +11,6 @@ Action RandomAgent::GetAction([[maybe_unused]]const Game& state)
     using Random = effolkronium::random_static;
 
     return static_cast<Action>(
-        Random::get(0, static_cast<int>(Action::COUNT)));
+        Random::get(0, static_cast<int>(Action::COUNT) - 1));
 }
 }  // namespace Baba
