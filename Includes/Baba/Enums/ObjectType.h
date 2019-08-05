2ea63e3d8cb6b30c9ddb4f7f5f080e5413e0dc43
HyeonSu Kim <rla92233@naver.com>
2019-08-05 20:43:46 +0900

    impl: Add EffectType

--- Includes/Baba/Enums/ObjectType.h
+++ Includes/Baba/Enums/ObjectType.h
@@ -14,18 +14,46 @@ enum class ObjectType
 {
     INVALID,
 #define TEXT(a) TEXT_##a,
+#define PROPERTY(a) TEXT_##a,
 #define BLOCK(a) a, TEXT_##a,
 #include "ObjectType.def"
 #undef TEXT
+#undef PROPERTY
 #undef BLOCK
+};
+
+enum class EffectType
+{
+    INVALID,
+#define TEXT(a)
+#define BLOCK(a) a,
+#define PROPERTY(a) a,
+#include "ObjectType.def"
+#undef TEXT
+#undef PROPERTY
+#undef BLOCK
+    COUNT,
 };
 
 const std::string OBJECT_TYPE_STR[] {
     "INVALID",
 #define TEXT(a) "TEXT_"#a,
+#define PROPERTY(a) "TEXT_"#a,
 #define BLOCK(a) #a, "TEXT_"#a,
 #include "ObjectType.def"
 #undef TEXT
+#undef PROPERTY
+#undef BLOCK
+};
+
+const std::string EFFECT_TYPE_STR[] {
+    "INVALID",
+#define TEXT(a)
+#define PROPERTY(a) #a,
+#define BLOCK(a) #a,
+#include "ObjectType.def"
+#undef TEXT
+#undef PROPERTY
 #undef BLOCK
 };
 }  // namespace Baba
