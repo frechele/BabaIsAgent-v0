bc7caabcbef0cfde8c386c55e288f3f00260a1be
HyeonSu Kim <rla92233@naver.com>
2019-08-08 19:25:32 +0900

    impl: Add WordClass and NOUN, VERB, PROPERTY TYPE STR

--- Includes/Baba/Enums/ObjectType.h
+++ Includes/Baba/Enums/ObjectType.h
@@ -4,7 +4,7 @@
 #define BABA_OBJECT_TYPE_H
 
 #include <string>
-#include <string_view>
+#include <vector>
 
 namespace Baba
 {
@@ -15,10 +15,12 @@ enum class ObjectType
 {
     INVALID,
 #define TEXT(a) TEXT_##a,
+#define VERB(a) TEXT_##a,
 #define PROPERTY(a) TEXT_##a,
 #define BLOCK(a) a, TEXT_##a,
 #include "ObjectType.def"
 #undef TEXT
+#undef VERB
 #undef PROPERTY
 #undef BLOCK
     COUNT,
@@ -26,6 +28,7 @@ enum class ObjectType
 
 enum class VerbType
 {
+    INVALID,
     IS,
     HAS,
     MAKE
@@ -34,36 +37,81 @@ enum class VerbType
 enum class EffectType
 {
     INVALID,
-    TEXT,
 #define TEXT(a)
+#define VERB(a) a,
 #define BLOCK(a) a,
 #define PROPERTY(a) a,
 #include "ObjectType.def"
 #undef TEXT
+#undef VERB
 #undef PROPERTY
 #undef BLOCK
     COUNT,
 };
 
+enum class WordClass
+{
+    INVALID,
+    NOUN,
+    VERB,
+    PROPERTY,
+};
+
 const std::string OBJECT_TYPE_STR[] {
     "INVALID",
 #define TEXT(a) "TEXT_"#a,
+#define VERB(a) "TEXT_"#a,
 #define PROPERTY(a) "TEXT_"#a,
 #define BLOCK(a) #a, "TEXT_"#a,
 #include "ObjectType.def"
 #undef TEXT
+#undef VERB
 #undef PROPERTY
 #undef BLOCK
 };
 
 const std::string EFFECT_TYPE_STR[] {
     "INVALID",
-    "TEXT",
 #define TEXT(a)
+#define VERB(a)#a,
 #define PROPERTY(a)#a,
 #define BLOCK(a)#a,
 #include "ObjectType.def"
 #undef TEXT
+#undef VERB
+#undef PROPERTY
+#undef BLOCK
+};
+
+const std::vector<std::string> NOUN_TYPE_STR {
+    "INVALID",
+#define TEXT(a)
+#define VERB(a)
+#define PROPERTY(a)
+#define BLOCK(a)#a,
+#include "ObjectType.def"
+#undef TEXT
+#undef VERB
+#undef PROPERTY
+#undef BLOCK
+};
+
+const std::vector<std::string> VERB_TYPE_STR {
+    "INVALID",
+    "IS",
+    "HAS",
+    "MAKE",
+};
+
+const std::vector<std::string> PROPERTY_TYPE_STR {
+    "INVALID",
+#define TEXT(a)
+#define VERB(a)
+#define PROPERTY(a)#a,
+#define BLOCK(a)
+#include "ObjectType.def"
+#undef TEXT
+
 #undef PROPERTY
 #undef BLOCK
 };
