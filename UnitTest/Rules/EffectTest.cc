6eeec0900009793db24cc8213eddeee30e6069fc
HyeonSu Kim <rla92233@naver.com>
2019-08-09 18:56:58 +0900

    refactor: Apply changes

--- UnitTest/Rules/EffectTest.cc
+++ UnitTest/Rules/EffectTest.cc
@@ -15,22 +15,22 @@ TEST(EffectTest, BABA)
 {
     Game game(5, 5);
 
-    Object& obj1 = game.Put(0, 0)
-                       .SetType(ObjectType::KEKE)
-                       .SetEffectType(EffectType::KEKE);
-    Object& obj2 = game.Put(1, 1)
-                       .SetType(ObjectType::STAR)
-                       .SetEffectType(EffectType::STAR);
+    Object& obj1 = game.Put(0, 0, "KEKE");
+    Object& obj2 = game.Put(1, 1, "STAR");
 
-    EXPECT_EQ(*game.FindObjectsByType(ObjectType::KEKE).at(0), obj1);
-    EXPECT_EQ(*game.FindObjectsByType(ObjectType::STAR).at(0), obj2);
+    game.Put(2, 1, "TEXT_KEKE");
+    game.Put(3, 1, "TEXT_IS");
+    game.Put(4, 1, "TEXT_BABA");
 
-    game.gameRules.AddBaseRule(EffectType::KEKE, EffectType::IS, EffectType::BABA);
+    EXPECT_EQ(*game.FindBlocksByName(ObjectName::KEKE).at(0), obj1);
+    EXPECT_EQ(*game.FindBlocksByName(ObjectName::STAR).at(0), obj2);
+
+    game.ParseRules();
 
     game.ApplyRules();
 
-    EXPECT_EQ(*game.FindObjectsByType(ObjectType::BABA).at(0), obj1);
-    EXPECT_EQ(*game.FindObjectsByType(ObjectType::STAR).at(0), obj2);
+    EXPECT_EQ(*game.FindBlocksByName(ObjectName::BABA).at(0), obj1);
+    EXPECT_EQ(*game.FindBlocksByName(ObjectName::STAR).at(0), obj2);
 }
 
 TEST(EffectTest, YOU)
@@ -45,36 +45,28 @@ TEST(EffectTest, WIN)
 
 TEST(EffectTest, MELT)
 {
-    Game game(5, 5);
-
-    Object& obj1 = game.Put(0, 0)
-                       .SetType(ObjectType::BABA)
-                       .SetEffectType(EffectType::BABA);
-
-    game.gameRules.AddBaseRule(EffectType::BABA, EffectType::IS, EffectType::MELT);
-
-    game.ApplyRules();
-
-    EXPECT_EQ(*game.FindObjectsByProperty(EffectType::MELT).at(0), obj1);
+    // Do nothing
 }
 
 TEST(EffectTest, HOT)
 {
     Game game(5, 5);
     
-    game.Put(0, 0)
-        .SetType(ObjectType::BABA)
-        .SetEffectType(EffectType::BABA);
-    game.Put(0, 0)
-        .SetType(ObjectType::KEKE)
-        .SetEffectType(EffectType::KEKE);
-   
-    game.gameRules.AddBaseRule(EffectType::BABA, EffectType::IS, EffectType::MELT);
-    game.gameRules.AddBaseRule(EffectType::KEKE, EffectType::IS, EffectType::HOT);
+    game.Put(0, 0, "BABA");
+    game.Put(0, 0, "KEKE");
 
+    game.Put(2, 1, "TEXT_BABA");
+    game.Put(3, 1, "TEXT_IS");
+    game.Put(4, 1, "TEXT_MELT");
+
+    game.Put(2, 2, "TEXT_KEKE");
+    game.Put(3, 2, "TEXT_IS");
+    game.Put(4, 2, "TEXT_HOT");
+   
+    game.ParseRules();
     game.ApplyRules();
 
-    EXPECT_TRUE(game.FindObjectsByProperty(EffectType::MELT).empty());
+    EXPECT_TRUE(game.FindObjectsByProperty(ObjectName::MELT).empty());
     EXPECT_EQ(game.At(0, 0).size(), 1);
-    EXPECT_EQ(game.At(0, 0).at(0)->GetType(), ObjectType::KEKE);
+    EXPECT_EQ(game.At(0, 0).at(0)->GetName(), ObjectName::KEKE);
 }
