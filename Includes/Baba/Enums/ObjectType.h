e9af99e2f0d7d93aaffd6caeb276466d7ad9b95e
HyeonSu Kim <rla92233@naver.com>
2019-08-09 17:13:08 +0900

    impl: Add UNIQUE_TYPE_STR

--- Includes/Baba/Enums/ObjectType.h
+++ Includes/Baba/Enums/ObjectType.h
@@ -12,11 +12,13 @@ enum class ObjectName
 {
     INVALID,
 #define TEXT(a)
+#define UNIQUE(a) a,
 #define VERB(a) a,
 #define BLOCK(a) a,
 #define PROPERTY(a) a,
 #include "ObjectType.def"
 #undef TEXT
+#undef UNIQUE
 #undef VERB
 #undef PROPERTY
 #undef BLOCK
@@ -29,37 +31,65 @@ enum class WordClass
     NOUN,
     VERB,
     PROPERTY,
+    UNIQUE,
 };
 
 const std::vector<std::string> NOUN_TYPE_STR {
     "INVALID",
 #define TEXT(a)
 #define VERB(a)
+#define UNIQUE(a)
 #define PROPERTY(a)
 #define BLOCK(a) #a,
 #include "ObjectType.def"
 #undef TEXT
 #undef VERB
+#undef UNIQUE
 #undef PROPERTY
 #undef BLOCK
 };
 
 const std::vector<std::string> VERB_TYPE_STR {
     "INVALID",
-    "IS",
-    "HAS",
-    "MAKE",
+#define TEXT(a)
+#define VERB(a) #a,
+#define UNIQUE(a)
+#define PROPERTY(a)
+#define BLOCK(a)
+#include "ObjectType.def"
+#undef TEXT
+#undef VERB
+#undef UNIQUE
+#undef PROPERTY
+#undef BLOCK
 };
 
 const std::vector<std::string> PROPERTY_TYPE_STR {
     "INVALID",
 #define TEXT(a)
 #define VERB(a)
+#define UNIQUE(a)
 #define PROPERTY(a) #a,
 #define BLOCK(a)
 #include "ObjectType.def"
 #undef TEXT
+#undef VERB
+#undef UNIQUE
+#undef PROPERTY
+#undef BLOCK
+};
 
+const std::vector<std::string> UNIQUE_TYPE_STR {
+    "INVALID",
+#define TEXT(a)
+#define VERB(a)
+#define UNIQUE(a) #a,
+#define PROPERTY(a)
+#define BLOCK(a)
+#include "ObjectType.def"
+#undef TEXT
+#undef VERB
+#undef UNIQUE
 #undef PROPERTY
 #undef BLOCK
 };
