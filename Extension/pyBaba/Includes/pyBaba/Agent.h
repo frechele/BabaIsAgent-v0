5eb15a4bcc103b89fccbd2888ac7a77d1ddebcf8
JYPark09 <jyp10987@gmail.com>
2019-08-16 16:37:47 +0900

    fix: Add ifndef at header files

--- Extension/pyBaba/Includes/pyBaba/Agent.h
+++ Extension/pyBaba/Includes/pyBaba/Agent.h
@@ -1,5 +1,10 @@
 // Copyright(C) 2019 Junyeong Park
 
+#ifndef PYBABA_AGENT_H
+#define PYBABA_AGENT_H
+
 #include <pybind11/pybind11.h>
 
 void buildAgents(pybind11::module& m);
+
+#endif  // PYBABA_AGENT_H
--- Extension/pyBaba/Includes/pyBaba/Game.h
+++ Extension/pyBaba/Includes/pyBaba/Game.h
@@ -1,7 +1,12 @@
 // Copyright(C) 2019 Junyeong Park
 
+#ifndef PYBABA_GAME_H
+#define PYBABA_GAME_H
+
 #include <pybind11/pybind11.h>
 
 void buildObject(pybind11::module& m);
 void buildRule(pybind11::module& m);
 void buildGame(pybind11::module& m);
+
+#endif  // PYBABA_GAME_H
