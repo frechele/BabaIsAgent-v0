fda54c4f7a5e2276c7d2abcc2e2562f33688602f
JYPark09 <jyp10987@gmail.com>
2019-08-21 00:59:19 +0900

    feat(py-api): Add Preprocess class

--- Extension/pyBaba/Sources/Agent.cc
+++ Extension/pyBaba/Sources/Agent.cc
@@ -3,10 +3,18 @@
 #include <pyBaba/Agent.h>
 
 #include <Baba/Agent/Agent.h>
+#include <Baba/Agent/Preprocess.h>
 #include <Baba/Agent/RandomAgent.h>
 
 namespace py = pybind11;
 using namespace Baba;
+
+void buildPreprocess(py::module& m)
+{
+    py::class_<Preprocess>(m, "Preprocess")
+        .def_static("StateToTensor", &Preprocess::StateToTensor)
+        .def_readonly_static("TENSOR_DIM", &Preprocess::TENSOR_DIM);
+}
 
 void buildAgents(py::module& m)
 {
