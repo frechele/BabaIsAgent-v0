b3e2ad42d97ff7ef82c0bd540785cbd6dfe56ff4
clwmrndl92 <clwmrndl92@gmail.com>
2019-08-21 20:30:09 +0900

    fix: change project skeleton

--- Extension/pyGUI/GUI.py
+++ Extension/pyGUI/GUI.py
@@ -2,8 +2,7 @@ import pygame
 import sys
 import pyBaba
 
-game = pyBaba.Game(10, 10)
-#test
+# test
 from pyBaba import Game, ObjectType
 
 game = Game(10, 10)
@@ -26,7 +25,6 @@ game.Put(9, 5).SetType(ObjectType.WIN)
 
 game.Put(8, 6).SetType(ObjectType.FLAG)
 
-#pyBaba.ObjectType.BABA
 # init
 pygame.init()
 pygame.font.init()
@@ -44,9 +42,9 @@ COLOR_BLACK = pygame.Color(0, 0, 0)
 COLOR_WHITE = pygame.Color(255, 255, 255)
 
 # image
-# ±◊≥… ≥Î∞°¥Ÿ∑Œ ¥Ÿ ∑ŒµÂ«ÿ≥ı¿⁄
-obj_images = {pyBaba.ObjectType.BABA : 'BABA', pyBaba.ObjectType.FLAG: 'FLAG', pyBaba.ObjectType.WALL: 'WALL'}
-text_images = {pyBaba.ObjectType.BABA : 'BABA', pyBaba.ObjectType.FLAG:  'FLAG', pyBaba.ObjectType.IS: 'IS',
+# Í∑∏ÎÉ• ÎÖ∏Í∞ÄÎã§Î°ú Îã§ Î°úÎìúÌï¥ÎÜìÏûê
+obj_images = {pyBaba.ObjectType.BABA: 'BABA', pyBaba.ObjectType.FLAG: 'FLAG', pyBaba.ObjectType.WALL: 'WALL'}
+text_images = {pyBaba.ObjectType.BABA: 'BABA', pyBaba.ObjectType.FLAG:  'FLAG', pyBaba.ObjectType.IS: 'IS',
                pyBaba.ObjectType.YOU:  'YOU', pyBaba.ObjectType.PUSH: 'PUSH', pyBaba.ObjectType.STOP: 'STOP',
                pyBaba.ObjectType.WALL: 'WALL', pyBaba.ObjectType.WIN: 'WIN'}
 for j in obj_images:
@@ -56,50 +54,77 @@ for j in obj_images:
             continue
         temp.append(pygame.transform.scale(pygame.image.load('./sprite/{}/{}.png'.format(obj_images[j], i)),
                                            (BLOCK_SIZE, BLOCK_SIZE)))
-    print(temp)
     obj_images[j] = temp
-    print(obj_images[j])
 
 for i in text_images:
     text_images[i] = pygame.transform.scale(pygame.image.load('./sprite/text/{}.png'.format(text_images[i])), (BLOCK_SIZE, BLOCK_SIZE))
-    print(text_images[i])
-print(obj_images)
 
-def DrawObject(obj, x_position, y_position):
-    if obj.IsText():
-        obj_image = text_images[obj.GetType()]
-    else:
-        print(obj.GetType())
-        obj_image = obj_images[obj.GetType()][int(game.GetNowAction())]
 
-    obj_rect = obj_image.get_rect()
-    obj_rect.topleft = (x_position * BLOCK_SIZE, y_position * BLOCK_SIZE)
+# object class
+class TempObjectClass(pygame.sprite.Sprite):
+    def __init__(self, obj):
+        pygame.sprite.Sprite.__init__(self)
+        self.obj = obj
+        if obj.IsText():
+            self.image = text_images[self.obj.GetType()]
+        else:
+            self.image = obj_images[self.obj.GetType()][int(game.GetNowAction())]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (game.GetPositionByObject(self.obj)[0] * BLOCK_SIZE, game.GetPositionByObject(self.obj)[1] * BLOCK_SIZE)
 
-    Screen.blit(obj_image, obj_rect)
+    def update(self):
+        if self.obj.IsDestroyed():
+            self.kill()
+        if self.obj.IsText():
+            self.image = text_images[self.obj.GetType()]
+        else:
+            self.image = obj_images[self.obj.GetType()][int(game.GetNowAction())]
+        self.rect.topleft = (game.GetPositionByObject(self.obj)[0] * BLOCK_SIZE, game.GetPositionByObject(self.obj)[1] * BLOCK_SIZE)
 
+
+object_group = pygame.sprite.Group()
 
 def IsObject(x_position, y_position):
     object_list = game.At(x_position, y_position)
     for obj in object_list:
-        DrawObject(obj, x_position, y_position)
-
+        object_group.add(TempObjectClass(obj))
 
 def Check():
     for y_position in range(game.GetHeight()):
         for x_position in range(game.GetWidth()):
             IsObject(x_position, y_position)
-
+Check()
+gameover =False
 
 while True:
+    if gameover:
+        pygame.quit()
+        sys.exit()
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+            if event.key == pygame.K_SPACE:
+                game.Update(pyBaba.Action.STAY)
+            if event.key == pygame.K_UP:
+                game.Update(pyBaba.Action.UP)
+            if event.key == pygame.K_DOWN:
+                game.Update(pyBaba.Action.DOWN)
+            if event.key == pygame.K_RIGHT:
+                game.Update(pyBaba.Action.RIGHT)
+            if event.key == pygame.K_LEFT:
+                game.Update(pyBaba.Action.LEFT)
 
-    Screen.fill(COLOR_WHITE)
+    # update
+    object_group.update()
+    if game.GetGameResult() == pyBaba.GameResult.WIN or game.GetGameResult() == pyBaba.GameResult.DEFEAT:
+        gameover = True
 
-    Check()
+    # draw
+    Screen.fill(COLOR_BLACK)
+    object_group.draw(Screen)
     pygame.display.flip()
 
     clock.tick(FPS)
