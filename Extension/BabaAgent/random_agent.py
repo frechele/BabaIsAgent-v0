00d2800b8054399376db937e4821e7258a78f5ca
JYPark09 <jyp10987@gmail.com>
2019-08-21 09:50:44 +0900

    fix: Fix codacy issue

--- Extension/BabaAgent/random_agent.py
+++ Extension/BabaAgent/random_agent.py
@@ -1,15 +1,17 @@
 import gym
-import environment
-from random import choice
+import environment as envi
+from pyBaba import RandomAgent
 
 env = gym.make('baba-10x10-v0')
 
 env.reset()
 
+agent = RandomAgent()
+
 current_reward = 0
 step = 0
 while not env.done:
-    state, reward, done, _ = env.step(choice(env.action_space))
+    state, reward, done, _ = env.step(agent.GetAction(env.game))
 
     current_reward += reward
     step += 1
